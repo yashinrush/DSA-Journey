88931d5f61012343c9153a232b62b2d3959feb5a
Yash Sonawane <157393874+yashinrush@users.noreply.github.com>
2026-06-22 15:30:05 +0530

    Time: 1 ms (59.42%), Space: 48.5 MB (31.38%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,28 +1,28 @@
 class Solution {
     public int trap(int[] height) {
         //calculate left max boundary = array
-        int n = height.length;
-        int leftMax[]= new int[n];
-        leftMax[0]=height[0];
-        for(int i=1;i<n;i++){
-            leftMax[i] = Math.max(height[i], leftMax[i-1]);
+        int n = height.length; //size of array
+        int leftMax[]= new int[n];//left max boundary array
+        leftMax[0]=height[0];//first element of left max boundary array is same as height array
+        for(int i=1;i<n;i++){//loop to calculate left max boundary array
+            leftMax[i] = Math.max(height[i], leftMax[i-1]);//left max boundary array is the maximum of the current height and the previous left max boundary
         }
 
         //calculate right max boundary = array
-        int rightMax[]=new int[n];
-        rightMax[n-1]=height[n-1];
-        for(int i = n-2; i>=0; i--){
-            rightMax[i]= Math.max(height[i], rightMax[i+1]);
+        int rightMax[]=new int[n];//right max boundary array
+        rightMax[n-1]=height[n-1];//last element of right max boundary array is same as height array
+        for(int i = n-2; i>=0; i--){//loop to calculate right max boundary array
+            rightMax[i]= Math.max(height[i], rightMax[i+1]);//right max boundary array is the maximum of the current height and the next right max boundary
         }
 
         //loop
-        int trappedWater=0;
-        for(int i = 0; i<n; i++){
+        int trappedWater=0;//variable to store the total trapped water
+        for(int i = 0; i<n; i++){//loop through the height array
             //waterleel = min(leftmax bound, rightmax bound)
-            int waterlevel = Math.min(leftMax[i], rightMax[i]);
+            int waterlevel = Math.min(leftMax[i], rightMax[i]);//water level is the minimum of the left max boundary and the right max boundary
             //trapped water = waterlevel - height[i]
-            trappedWater += waterlevel - height[i]; 
+            trappedWater += waterlevel - height[i]; //trapped water is the difference between the water level and the current height
         }
-        return trappedWater;
+        return trappedWater;//return the total trapped water
     }
 }
